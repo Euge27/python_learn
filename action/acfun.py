cecbb6a621ca398e2bae6ed53bb06e237fcdfac0
fancylinq <fancylinq@163.com>
2013-06-16 19:25:15 +0800

    add acfun player

---
+++ action/acfun.py
@@ -0,0 +1,32 @@
+import re
+import json
+import time
+import urllib.request
+import lxml.etree
+from pyquery import PyQuery as pyq
+
+def findApiId(url):
+  doc = pyq(url=r'{}'.format(url))
+  text = doc('#area-player div.hidden').text()
+  result = re.search('\d+', text).group()
+  return result
+
+def findVid(url):
+  data = urllib.request.urlopen(url).read()
+  string = data.decode('utf-8')
+  jobj = json.loads(string)
+  return jobj['vid']
+
+def analyzePlaylist(url):
+  data = urllib.request.urlopen(url).read()
+  tree = lxml.etree.fromstring(data)
+  return [ele.text for ele in tree.findall('durl/url')]
+
+def getPlaylist(url):
+  acId = findApiId(url)
+  vid = findVid('http://www.acfun.tv/api/player/vids/{}.aspx'.format(acId))
+  list = analyzePlaylist('http://v.iask.com/v_play.php?vid={}&dtime={}'.format(vid, round(time.time() * 1000)))
+  return list
+
+if __name__ == '__main__':
+  print getPlaylist('')
